92ce487a0c33478d9a12241b1438abc001ca2076
h7ml <h7ml@qq.cn>
2024-05-16 18:59:37 +0800

    ✨ feat(store): Redux-saga

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,8 +2,20 @@ import { configureStore, ThunkAction, Action, Middleware } from '@reduxjs/toolki
 import counterReducer from '../features/counter/counterSlice';
 import todoReducer from '../features/todos/todoSlice';
 import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
+
+// 创建 redux-saga 中间件实例
+const sagaMiddleware = createSagaMiddleware();
+
+// 创建根 Saga 函数
+function* rootSaga() {
+  // 在此处放置你的所有 saga 函数
+  yield all([
+    // saga 函数列表
+  ]);
+}
 
 export const store = configureStore({
   reducer: {
@@ -15,9 +27,12 @@ export const store = configureStore({
     if (isDevelopment) {
       middleware.push(logger as Middleware<{}, any, any>);
     }
+    middleware.push(sagaMiddleware);
     return middleware;
   },});
 
+  // 运行根 Saga 函数
+sagaMiddleware.run(rootSaga);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
@@ -26,3 +41,4 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
