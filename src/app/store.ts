2a8a84663d21746fa2fd851984e862e880a8f9bc
h7ml <h7ml@qq.cn>
2024-05-16 18:56:04 +0800

    ✨ feat(redux): Redux-logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,22 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, Middleware } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import todoReducer from '../features/todos/todoSlice';
+import logger from 'redux-logger'
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     todos: todoReducer,
   },
-});
+  middleware: (getDefaultMiddleware) => {
+    const middleware = getDefaultMiddleware();
+    if (isDevelopment) {
+      middleware.push(logger as Middleware<{}, any, any>);
+    }
+    return middleware;
+  },});
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
